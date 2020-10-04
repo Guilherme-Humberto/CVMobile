f9de8604f69f95d1dc089aee6aefe5d370d5f6a9
guilherme-humberto <gbreu@outlook.com>
2020-10-04 01:36:50 -0300

    Arquivo principal

---
+++ App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Routes } from './src/routes/routes.js'
+import * as Font from 'expo-font'
+import { AppLoading } from 'expo';
+
+const getFonts = () => {
+  return Font.loadAsync({
+    "Lora": require('./assets/fonts/Lora-VariableFont_wght.ttf'),
+    "Roboto": require('./assets/fonts/Roboto-Bold.ttf'),
+    "Cookie": require('./assets/fonts/Cookie-Regular.ttf'),
+    "Alata": require('./assets/fonts/Alata-Regular.ttf')
+  })
+}
+
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false)
+
+  if(fontsLoaded) {
+    return (
+      <>
+        <StatusBar style="auto" />
+        <Routes />
+      </>
+    )
+  }
+  else {
+    return (
+      <AppLoading 
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    )
+  }
+}
+    
+    
