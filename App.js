4b98c1355e2cd30b1cac0519aea0b5ca5cefc7d8
Guilherme-Humberto <gbreu@outlook.com>
2020-10-29 15:26:30 -0300

    Arquivo principal

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes } from './src/routes/routes.js'
 import * as Font from 'expo-font'
 import { AppLoading } from 'expo';
