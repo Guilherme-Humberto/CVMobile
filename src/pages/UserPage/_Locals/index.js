7c5e4c5405b0ec817f9d027c4abc02534de2aee0
Guilherme Humberto <gbreu@outlook.com>
2020-12-09 22:00:06 -0300

    Páginas

--- src/pages/UserPage/_Locals/index.js
+++ src/pages/UserPage/_Locals/index.js
@@ -1,20 +1,14 @@
 import React, { useState, useEffect } from "react";
-import * as Permissions from "expo-permissions";
-import { usePermissions } from "@use-expo/permissions";
 import MapView, { Marker } from "react-native-maps";
 import {
   StyleSheet,
   ActivityIndicator,
   Dimensions,
-  Text,
-  View,
   Alert,
-  Button,
 } from "react-native";
 import { Container } from "./styles";
 import Fetcher from "../../../hooks/Fetcher";
 import Modal from "../../../components/Modals/ModalMap/index";
-import LoadMessage from "../../../components/LoadMessage";
 
 // Posição inicial do usuário
 // latitudeDelta e longitudeDelta: referentes ao zoom
@@ -26,10 +20,6 @@ const initialPosition = {
 };
 
 const Locals = () => {
-  const [permission, askForPermission] = usePermissions(Permissions.LOCATION, {
-    ask: true,
-  });
-
   const { data } = Fetcher("institutions");
   // Pegando a largura e a altura da tela
   const { width, height } = Dimensions.get("window");
@@ -84,46 +74,31 @@ const Locals = () => {
     ));
   };
 
-  if (!permission || permission.status !== "granted") {
-    Alert.alert(
-      `Aviso`,
-      `Para visualizar os hemocentros mais próximos de você, precisamos que você permita o acesso aos de dados de localização`,
-      [
-        { text: "Permitir", onPress: () => askForPermission() },
-        { text: "Fechar", style: "cancel" },
-      ],
-      {
-        cancelable: true,
-      }
-    );
-  } 
-  if (!data) {
-    return null
-  }
-    return (
-      <>
-        <Container>
-          {currentPosition.latitude ? (
-            <MapView
-              style={{ width, height }}
-              initialRegion={currentPosition}
-              showsPointsOfInterest={false}
-              showsBuildings={false}
-              showsUserLocation
-            >
-              {renderMarker()}
-            </MapView>
-          ) : (
-            <ActivityIndicator style={{ flex: 1 }} animating size="large" />
-          )}
-        </Container>
+  if (!data) return null;
 
-        {openModal && (
-          <Modal closeModal={() => setModalOpen(false)} infos={infos} />
+  return (
+    <>
+      <Container>
+        {currentPosition.latitude ? (
+          <MapView
+            style={{ width, height }}
+            initialRegion={currentPosition}
+            showsPointsOfInterest={false}
+            showsBuildings={false}
+            showsUserLocation
+          >
+            {renderMarker()}
+          </MapView>
+        ) : (
+          <ActivityIndicator style={{ flex: 1 }} animating size="large" />
         )}
-      </>
-    );
-  
+      </Container>
+
+      {openModal && (
+        <Modal closeModal={() => setModalOpen(false)} infos={infos} />
+      )}
+    </>
+  );
 };
 
 export default Locals;
